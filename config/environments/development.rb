a55e52480070cf3fb7c5da526112f616604a0598
Syhola <syho.webmaster@gmail.com>
2016-11-02 13:45:41 +0000

    Prep for launch

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # Add Cloud9 origin for Action Cable requests.
+  config.action_cable.allowed_request_origins = ['http://workspace-syho.c9users.io:8080']
 end
